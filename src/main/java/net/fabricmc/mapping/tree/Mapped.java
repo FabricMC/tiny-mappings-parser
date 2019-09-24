8fa1e5c2987c3ffb0b32474637127987148ae2f7
liach <liach@users.noreply.github.com>
2019-09-23 19:50:18 -0500

    Forgot to document a class
    Signed-off-by: liach <liach@users.noreply.github.com>

--- src/main/java/net/fabricmc/mapping/tree/Mapped.java
+++ src/main/java/net/fabricmc/mapping/tree/Mapped.java
@@ -1,10 +1,35 @@
 package net.fabricmc.mapping.tree;
 
+/**
+ * Represents an element that can be mapped and have comments.
+ */
 public interface Mapped {
 
+	/**
+	 * Gets the mapped name of the element in the target namespace.
+	 *
+	 * @param namespace the target namespace
+	 * @return the mapped name
+	 */
 	String getName(String namespace);
 
+	/**
+	 * Gets an explicitly mapped name of the element in the target namespace.
+	 *
+	 * <p>May be {@code null} if the target namespace inherits the name from another
+	 * namespace.
+	 *
+	 * @param namespace the target namespace
+	 * @return the explicitly mapped name
+	 */
 	/* Nullable */ String getMappedName(String namespace);
 
+	/**
+	 * Gets the comment (JavaDoc, etc.) on an element.
+	 *
+	 * <p>The comment may be {@code null} if there is none.
+	 *
+	 * @return the comment or {@code null}
+	 */
 	/* Nullable */ String getComment();
 }
