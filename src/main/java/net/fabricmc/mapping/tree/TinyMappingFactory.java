6db038a990c2dff507cb1b977706e2564c4dabc1
natanfudge <natan.lifsiz@gmail.com>
2019-09-26 15:30:14 +0300

    forgot to add instance

--- src/main/java/net/fabricmc/mapping/tree/TinyMappingFactory.java
+++ src/main/java/net/fabricmc/mapping/tree/TinyMappingFactory.java
@@ -14,6 +14,8 @@ import java.util.function.ToIntFunction;
  */
 public final class TinyMappingFactory {
 
+	public static final TinyTree EMPTY_TREE = new EmptyTinyTree();
+
 	/**
 	 * Loads a tree model from a buffered reader for v2 input.
 	 *
