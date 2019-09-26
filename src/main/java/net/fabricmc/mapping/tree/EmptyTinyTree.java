123822bfe0c9a2a6ceeb577807da69843d1e3985
natanfudge <natan.lifsiz@gmail.com>
2019-09-26 15:14:26 +0300

    empty tiny tree

---
+++ src/main/java/net/fabricmc/mapping/tree/EmptyTinyTree.java
@@ -0,0 +1,52 @@
+package net.fabricmc.mapping.tree;
+
+import net.fabricmc.mapping.reader.v2.TinyMetadata;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class EmptyTinyTree implements TinyTree {
+    @Override
+    public TinyMetadata getMetadata() {
+        return new TinyMetadata() {
+            @Override
+            public int getMajorVersion() {
+                return 2;
+            }
+
+            @Override
+            public int getMinorVersion() {
+                return 0;
+            }
+
+            @Override
+            public List<String> getNamespaces() {
+                return Collections.emptyList();
+            }
+
+            @Override
+            public Map<String, @Nullable String> getProperties() {
+                return Collections.emptyMap();
+            }
+
+            @Override
+            public int index(String namespace) throws IllegalArgumentException {
+                return 0;
+            }
+        };
+    }
+
+    @Override
+    public Map<String, ClassDef> getDefaultNamespaceClassMap() {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public Collection<ClassDef> getClasses() {
+        return Collections.emptyList();
+    }
+}
